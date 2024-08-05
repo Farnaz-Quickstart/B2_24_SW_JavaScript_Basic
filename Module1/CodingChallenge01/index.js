3b50170b39c3a363a344c14c5299a28e8965aa5f
farnaz <farnaz.towhidi@gmail.com>
2024-08-05 08:38:22 -0400

    Module2-Exercise2, check text length

---
+++ Module1/CodingChallenge01/index.js
@@ -0,0 +1,16 @@
+// Primitive data types (Numbers, Strings, Booleans, Undefined, Null, BigInt, Symbol)
+
+let num = 3;
+let str = 'abc';
+let bool = true;
+let undef = undefined;
+let nullValue = null;
+let bigNum = 5n;
+let mySymbol = Symbol('test'); 
+
+
+// Composite data Types (Functions, Arrays, Objects, Maps, Sets)
+let sum = (a, b) => a + b;
+let dict = {name: 'John', age: 30};
+let mySet = new Set([1, 2, 3, 3, 4]);
+let myMap = new Map([[1, 'one'], [2, 'two']]); 
--- Module2/Exercise1/index.js
+++ Module2/Exercise1/index.js
@@ -1,3 +1,7 @@
+// Exercise 1: 
+// Write a program that prompts the user to input a number and checks whether the number is even or odd. 
+// Print "Even" if the number is even and "Odd" if the number is odd.
+
 let value = prompt("Enter an integer:");
 let intValue = parseInt(value);
 
