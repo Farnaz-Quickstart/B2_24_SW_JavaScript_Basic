31bcbd1c1d1d5714c7c5f94eee3419a380e84aac
farnaz <farnaz.towhidi@gmail.com>
2024-08-23 21:37:59 -0400

    Update coding challenge

---
+++ Module2/CodingChallenge4/index.js
@@ -0,0 +1,5 @@
+// 4. Think of a number between 1 and 100, but don't tell this to the computer.
+// Once the computer guesses a number based on the rules seen in exercise 3,
+// tell the computer if your number is greater than, less than, or equal to the
+// guess of the computer. The computer continues guessing until it guesses your
+// number correctly.
